a500c63f4f12b124c5dc6011633c05e0f580a3ba
AllSafeCybercurity <skull32273227@gmail.com>
2023-04-17 07:15:47 -0700

    `openai`

--- chatgpt/chatbot/urls.py
+++ chatgpt/chatbot/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.index, name="index"),
+    path('', views.index, name="index"),
+    path('past', views.past, name="past"),
+    path('delete_past/<Past_id>', views.delete_past, name="delete_past"),
+
 ]
